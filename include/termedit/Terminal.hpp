b0a6f479588ea6ea9ff28185cb5cdd5b9afe65f2
Tye Hoshizaki <tyehoshizaki@gmail.com>
2026-03-22 18:14:41 -0700

    added minimal termial class

---
+++ include/termedit/Terminal.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <utility>
+
+namespace termedit {
+
+class Terminal {
+public:
+  Terminal();
+  ~Terminal();
+
+  Terminal(const Terminal &) = delete;
+  Terminal &operator=(const Terminal &) = delete;
+
+  void enableRawMode();
+  void disableRawMode();
+
+  [[nodiscard]] char readKey() const;
+  [[nodiscard]] std::pair<int, int> getWindowsSize() const;
+
+  void write(const std::string &text) const;
+  void clearScreen() const;
+
+private:
+  bool rawModeEnabled_ = false;
+};
+
+} // namespace termedit
