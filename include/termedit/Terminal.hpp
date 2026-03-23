b184d10098db4bb27eba31bf6fa80af2d8574fe6
Tye Hoshizaki <tyehoshizaki@gmail.com>
2026-03-23 12:56:38 -0700

    added raw functionality and ctrl-q works

--- include/termedit/Terminal.hpp
+++ include/termedit/Terminal.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
 #include <string>
-#include <sys/ioctl.h>
-#include <unistd.h>
+#include <termios.h>
 #include <utility>
 
 namespace termedit {
@@ -19,12 +18,13 @@ public:
   void disableRawMode();
 
   [[nodiscard]] char readKey() const;
-  [[nodiscard]] std::pair<int, int> getWindowsSize() const;
+  [[nodiscard]] std::pair<int, int> getWindowSize() const;
 
   void write(const std::string &text) const;
   void clearScreen() const;
 
 private:
+  termios orig_termios{};
   bool rawModeEnabled_ = false;
 };
 
